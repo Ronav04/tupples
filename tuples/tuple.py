a2e292086c39595db7e90ad3b1896501fd1821bd
Ronav04 <ropatra23@alcestergs.com>
2024-11-26 19:57:30 +0000

    Add files via upload

---
+++ tuples/tuple.py
@@ -0,0 +1,23 @@
+
+
+
+
+numbers = (10,20,30,40,-50)
+
+#print(numbers)
+
+#print(numbers[1])
+
+#print(len(numbers))
+
+#for i in numbers:
+    #print(i)
+    
+#del numbers
+
+#print(numbers)   
+
+colours = ("red","blue","orange","green","purple") 
+
+print("green" in colours)
+print("yellow" in colours)
